351210666b25b5ce61de4dcfc82bfae0e4df1754
Joel <joelulahanna@gmail.com>
2020-12-29 10:47:30 -0600

    add multip happs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { getAppToInstall } from "./args";
 import { execHolochain } from "./execHolochain.js";
 import { installApp, attachAppInterface } from "./installApp.js";
 import getPort from "get-port";
+import fs from "fs";
+const yaml = require('js-yaml');
 
 async function execAndInstall(appToInstall) {
 
@@ -16,7 +18,14 @@ async function execAndInstall(appToInstall) {
   // If the config file was created assume we also need to install everything
   if (configCreated) {
     await sleep(100);
-    await installApp(realAdminPort, appToInstall.appPort,  appToInstall.dnas, appToInstall.installedAppId);
+    if (appToInstall.happs) {
+      const happs = yaml.safeLoad(fs.readFileSync(appToInstall.happs, 'utf8'));
+      happs.map(async happ => {
+        await installApp(realAdminPort, happ.app_port,  happ.dnas, happ.app_name);
+      });
+    } else {
+      await installApp(realAdminPort, appToInstall.appPort,  appToInstall.dnas, appToInstall.installedAppId);
+    }
   } else {
     await sleep(500);
     await attachAppInterface(realAdminPort, appToInstall.appPort, appToInstall.installedAppId);
