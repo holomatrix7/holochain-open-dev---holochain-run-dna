1dd52dba5796b5eff0754401d554e1c064cc5255
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-09 21:34:17 +0100

    Added nix

---
+++ default.nix
@@ -0,0 +1,42 @@
+# This is an example of what downstream consumers of holonix should do
+# This is also used to dogfood as many commands as possible for holonix
+# For example the release process for holonix uses this file
+let
+
+ # point this to your local config.nix file for this project
+ # example.config.nix shows and documents a lot of the options
+ config = import ./config.nix;
+
+ # START HOLONIX IMPORT BOILERPLATE
+ holonix = import (
+  if ! config.holonix.use-github
+  then config.holonix.local.path
+  else fetchTarball {
+   url = "https://github.com/${config.holonix.github.owner}/${config.holonix.github.repo}/tarball/${config.holonix.github.ref}";
+   sha256 = config.holonix.github.sha256;
+  }
+ ) { 
+   config = config;
+
+   holochainVersionId = "custom";
+   
+   holochainVersion = { 
+    rev = "8c62cb5888f491f08e6fdf13b6f3991f85a9801a";  
+    sha256 = "1kgxyfrwmga27mqywhivn0xdi6br90bavqvnd4kbrfyzbzmf8fcr";  
+    cargoSha256 = "1ix8ihlizjsmx8xaaxknbl0wkyck3kc98spipx5alav8ln4wf46s";
+   };
+ };
+ # END HOLONIX IMPORT BOILERPLATE
+
+in
+with holonix.pkgs;
+{
+ dev-shell = stdenv.mkDerivation (holonix.shell // {
+  name = "dev-shell";
+
+  buildInputs = [ ]
+   ++ holonix.shell.buildInputs
+   ++ config.buildInputs
+  ;
+ });
+}
